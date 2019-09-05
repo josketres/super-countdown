18da4c51491c93c885031ba7cd2665c175f39e79
Lidia Chebotarova <lidia.chebotarova@lotto24.de>
2019-09-05 12:12:49 +0200

    iteration-4a show the data on button click

--- weather.js
+++ weather.js
@@ -1,42 +1,60 @@
 //fetch weather data:
 
 function fetchData() {
-    return fetch('http://api.tvmaze.com/shows/1/episodes')
-        .then(function(response) {
-            return response.json();
-        })
-        .then(data => {
-            return data.map((item) => ({
-                name: item.name,
-                url: item.url,
-                runtime: item.runtime,
-                airdate: item.airdate,
-                airtime: item.airtime
-            }))
-        })
-        // .catch(error => console.error('Error:', error))
-        .then(data => {
-            console.log('Success:', data)
-            return data;
-        });
+  return (
+    fetch("http://api.tvmaze.com/shows/1/episodes")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(data => {
+        return data.map(item => ({
+          name: item.name,
+          url: item.url,
+          runtime: item.runtime,
+          airdate: item.airdate,
+          airtime: item.airtime
+        }));
+      })
+      // .catch(error => console.error('Error:', error))
+      .then(data => {
+        console.log("Success:", data);
+        return data;
+      })
+  );
 }
-    
-const btn = document.getElementById('button');
-btn.addEventListener('click', handler);
+
+const btn = document.getElementById("button");
+btn.addEventListener("click", handler);
 
 function showData(arr) {
-    console.log(arr);
-    let div = document.querySelector('.container');
-    for(let i = 0; i < arr.length; i++) {
-    let items = document.createElement('div');
-    items.id = 'item';
-    items.classList.add('wrap');
-    items.innerHTML=`<div>${arr[i]}</div>`;
-    div.appendChild(items); } 
+  let div = document.querySelector(".container");
+  for (const item of arr) {
+    let itemElem = document.createElement("div");
+    div.appendChild(itemElem);
+    for (let [key, value] of Object.entries(item)) {
+      let newElem = document.createElement("div");
+      newElem.innerText = `${key} : ${value}`;
+      console.log(`${key}: ${value}`);
+      itemElem.appendChild(newElem);
+    }
+    //another way using for in loop: 
+    /* for (const prop in item) {
+      let newElem = document.createElement("div");
+      newElem.innerText = `${prop} : ${item[prop]}`;
+      itemElem.appendChild(newElem);
+    } */
+  }
 }
 
-/**
- 
+
+function handler() {
+  fetchData().then(showData);
+}
+
+
+/*
+ Task: 
+
 1. create handler function for the click event
 2. in that function, call the fetchData function
 3. show the data when the fetchData promise resolves
@@ -53,12 +71,8 @@ function showData(arr) {
 }
 5. in your showData function, create a separate div for every item in the array.
 6. in each of these divs, create another div for each property on the item, with key and value as the content
+(read about: object key, object value, object entry);
 
 <div>
  */
-
-function handler() {
-    fetchData()
-    .then(showData);
-}
 
